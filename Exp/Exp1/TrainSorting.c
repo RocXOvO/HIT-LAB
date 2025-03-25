b75319be450cead8607eee3dac1874be037f2265
鲤鱼王 <1376107905@qq.com>
2025-03-25 21:41:15 +0800

    Finish almost all.

---
+++ Exp/Exp1/TrainSorting.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack{
+    int *data;
+    int top;
+    int capacity;
+}Stack;
+
+typedef struct quene{
+    int *data;
+    int front;
+    int last;
+    int capacity;
+}Quene;
+
+/**Create a new Stack.
+ * capacity means the capacity.
+ */
+Stack* CreatStack(int capacity){
+    Stack *newStack = malloc(sizeof(Stack));
+    if (newStack == NULL){
+        printf("内存分配错误");
+        exit(1);
+    }
+    newStack -> top = -1;
+    newStack -> capacity = capacity;
+    newStack -> data = malloc(sizeof(int) * newStack -> capacity);
+    if (newStack -> data == NULL){
+        printf("内存分配错误");
+        exit(1);
+    }
+    return newStack;
+}
+
+int isEmpty(Stack *stack){
+    return stack -> top == -1;
+}
+
+int pushStack(Stack *stack, int value){
+    if (stack -> top > stack -> capacity -1){
+        printf("栈满");
+        return 0;
+    }
+    stack -> data[++stack -> top] == value;
+    return value;
+}
+
+int popStack(Stack *stack){
+    if(isEmpty){
+        printf("栈空");
+        return 0;
+    }
+    int value = stack -> data[stack -> top--];
+    return value;
+}
+
+int main(){
+    return 0;
+}
